76386ea2a48531d29bb2a22bdf1f2c31ff0087ba
ilugobayo <ivan.lugo@bayonet.io>
2020-09-27 05:32:11 -0600

    Adds general functions
    Adds general functions, this is, a general function to get data from a specific element, in this way, instead of writing all that code again, each new function for a specific value will call one of these functions, sending the parameters for the query in as parameters

--- Helper/DirectQuery.php
+++ Helper/DirectQuery.php
@@ -17,17 +17,87 @@ class DirectQuery
     }
 
     /**
-     * Gets the current value for the backfill mode
+     * Performs a direct query to the database to retrieve the requested
+     * configuration value of the module
+     * 
+     * @param string $table
+     * @param string $requestedValue
+     * @param string $configPath
+     * @return int
      */
-    public function getBackfillMode()
+    protected function configQuery($table, $requestedValue, $configPath)
     {
         $connection  = $this->resourceConnection->getConnection();
-        $tableName = $connection->getTableName('core_config_data');
-        $query = $connection->select()->from($tableName, 'value')->where('path = :path');
-        $path = 'bayonetantifraud_general/general/backfill_mode';
+        $tableName = $connection->getTableName($table);
+        $query = $connection->select()->from($tableName, $requestedValue)->where('path = :path');
+        $path = 'bayonetantifraud_general/general/'.$configPath;
         $bind = [':path' => (string)$path];
         $result = (int)$connection->fetchOne($query, $bind);
 
         return $result;
+    }
+
+    /**
+     * Performs a direct query to the database to retrieve the requested
+     * value of an order
+     * 
+     * @param string $table
+     * @param string $requestedValue
+     * @param string $orderId
+     * @return string
+     */
+    public function orderQuery($table, $requestedValue, $orderId)
+    {
+        $connection  = $this->resourceConnection->getConnection();
+        $tableName = $connection->getTableName($table);
+        $query = $connection->select()->from($tableName, $requestedValue)->where('order_id = :orderId');
+        $bind = [':orderId' => $orderId];
+        $result = $connection->fetchOne($query, $bind);
+
+        return $result;
+    }
+
+    /**
+     * Performs a direct query to the database to retrieve the requested
+     * value of a customer
+     * 
+     * @param string $table
+     * @param string $requestedValue
+     * @param string $customerId
+     * @return string
+     */
+    protected function customerQuery($table, $requestedValue, $customerId)
+    {
+        $connection  = $this->resourceConnection->getConnection();
+        $tableName = $connection->getTableName($table);
+        $query = $connection->select()->from($tableName, $requestedValue)->where('customer_id = :customerId');
+        $bind = [':customerId' => $customerId];
+        $result = $connection->fetchOne($query, $bind);
+
+        return $result;
+    }
+    
+    /**
+     * Gets the current value of the enable configuration for the module
+     * 
+     * @return int
+     */
+    public function getEnabled()
+    {
+        $enabled = $this->configQuery('core_config_data', 'value', 'enabled');
+
+        return $enabled;
+    }
+
+    /**
+     * Gets the current value for the API mode
+     * 
+     * @return int
+     */
+    public function getApiMode()
+    {
+        $apiMode = $this->configQuery('core_config_data', 'value', 'api_mode');
+
+        return $apiMode;
     }
 }
