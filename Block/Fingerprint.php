89dd88eef486842158040e654345e8eb6a2b9c22
ilugobayo <ivan.lugo@bayonet.io>
2020-09-28 03:57:05 -0600

    Adds the files to add fingerprint script to the checkout
    Adds the required files to add the fingerprint script to the checkout (layout, template & block)

---
+++ Block/Fingerprint.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Bayonet\BayonetAntiFraud\Block;
+
+use \Magento\Framework\View\Element\Template\Context;
+use \Magento\Framework\View\Element\Template;
+use \Bayonet\BayonetAntiFraud\Helper\DirectQuery;
+
+/**
+ * Block class used to get data in the Fingerprint template
+ */
+class Fingerprint extends Template
+{
+    protected $_isScopePrivate = false;
+    protected $directQuery;
+    protected $httpContext;
+
+    public function __construct(
+        Context $context,
+        \Magento\Framework\App\Http\Context $httpContext,
+        DirectQuery $directQuery,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->directQuery = $directQuery;
+        $this->httpContext = $httpContext;
+    }
+
+    /**
+     * Gets the controller URL
+     * 
+     * @return string
+     */
+    public function getAjaxUrl()
+    {
+        return $this->getUrl('device/fingerprint/index');
+    }
+
+    /**
+     * Gets the current value of the 'enable' configuration field
+     * 
+     * @return int
+     */
+    public function getEnabled()
+    {
+        return intval($this->directQuery->getEnabled());
+    }
+
+    /**
+     * Gets the API key in order to generate the fingerprint token
+     * It checks whether the the API mode is in live or sandbox in order
+     * to return the corresponding key
+     * 
+     * @return string
+     */
+    public function getApiKey()
+    {
+        $apiMode = intval($this->directQuery->getApiMode());
+        $apiKey = $apiMode === 1 ? $this->directQuery->getApiKey('js_live_key') : $this->directQuery->getApiKey('js_sandbox_key');
+        
+        return $apiKey;
+    }
+
+    /**
+     * Gets a bool that indicates whether a customer is logged in or not
+     * 
+     * @return bool
+     */
+    public function getCustomerIsLoggedIn()
+    {
+    	return (bool)$this->httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_AUTH);
+    }
+
+    /**
+     * Gets the ID of the logged in customer
+     * 
+     * @return string
+     */
+    public function getCustomerId()
+    {
+    	return $this->httpContext->getValue('customer_id');
+    }
+}
