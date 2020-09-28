a5133a7e9d526c949aef14eea5f8dc50b0a0ef84
ilugobayo <ivan.lugo@bayonet.io>
2020-09-28 00:49:39 -0600

    Adds Bayonet blocklist table management files
    Adds the model, resource model & collection files for the management of the Bayonet blocklist table in the database.

---
+++ Model/ResourceModel/BayonetBlocklist/Collection.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Bayonet\BayonetAntiFraud\Model\ResourceModel\BayonetBlocklist;
+
+use \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
+/**
+ * Defines the Collection for the Bayonet Blocklist table
+ */
+class Collection extends AbstractCollection
+{
+    protected $_idFieldName = 'blocklist_id';
+    protected $_eventPrefix = 'bayonet_antifraud_blocklist_collection';
+    protected $_eventObject = 'blocklist_collection';
+
+    /**
+     * Defines the resource model
+     */
+    protected function _construct()
+    {
+        $this->_init(
+            'Bayonet\BayonetAntiFraud\Model\BayonetBlocklist',
+            'Bayonet\BayonetAntiFraud\Model\ResourceModel\BayonetBlocklist'
+        );
+    }
+
+    /**
+     * Get SQL for get record count.
+     * Extra GROUP BY strip added.
+     *
+     * @return \Magento\Framework\DB\Select
+     */
+    public function getSelectCountSql()
+    {
+        $countSelect = parent::getSelectCountSql();
+        $countSelect->reset(\Zend_Db_Select::GROUP);
+        return $countSelect;
+    }
+    /**
+     * @param string $valueField
+     * @param string $labelField
+     * @param array $additional
+     * @return array
+     */
+    protected function _toOptionArray($valueField = 'blocklist_id', $labelField = 'name', $additional = [])
+    {
+        return parent::_toOptionArray($valueField, $labelField, $additional);
+    }
+}
